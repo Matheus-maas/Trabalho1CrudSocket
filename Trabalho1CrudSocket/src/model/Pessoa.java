e7e00fad28964dc22445ed2959df8453a6235cb7
matheus-maas <matheus.h.maas@hotmail.com>
2021-05-27 12:53:28 -0300

    atributos classe Pessoa

--- Trabalho1CrudSocket/src/model/Pessoa.java
+++ Trabalho1CrudSocket/src/model/Pessoa.java
@@ -3,5 +3,33 @@ package model;
 
 
 public class Pessoa {
+    String cpf;
+    String nome;
+    String endereco;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
     
 }
