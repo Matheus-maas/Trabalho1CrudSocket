abca4cecc64dd74c030282d36b2f66a1706ce984
matheus-maas <matheus.h.maas@hotmail.com>
2021-05-27 12:53:15 -0300

    atributos classe AluguelCarro

--- Trabalho1CrudSocket/src/model/AluguelCarro.java
+++ Trabalho1CrudSocket/src/model/AluguelCarro.java
@@ -1,7 +1,37 @@
 
 package model;
 
+import java.util.Date;
+
 
 public class AluguelCarro {
+    Double preco;
+    Date dataAluguel;
+    Date dataVencimento;
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Date getDataAluguel() {
+        return dataAluguel;
+    }
+
+    public void setDataAluguel(Date dataAluguel) {
+        this.dataAluguel = dataAluguel;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+    
     
 }
