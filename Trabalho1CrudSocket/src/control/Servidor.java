cb20d59aa39059d53fe442eae947e0914b9fdfba
Lucas de Liz Frutuoso <Dell@DESKTOP-UF07HOB>
2021-06-05 11:17:54 -0300

    sistema de denuncia 01

--- Trabalho1CrudSocket/src/control/Servidor.java
+++ Trabalho1CrudSocket/src/control/Servidor.java
@@ -1,4 +1,3 @@
-
 package control;
 
 import java.io.IOException;
@@ -6,11 +5,10 @@ import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-
 public class Servidor {
 
-    public static void main(String[] args) throws IOException{
-       System.out.println("Criando Conexão....");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Criando Conexão....");
         try {
             Socket conn = new Socket("127.0.0.1", 80);
             System.out.println("Conectado!");
@@ -20,8 +18,14 @@ public class Servidor {
             int qtdBytesLidos = in.read(dadosBrutos);
             while (qtdBytesLidos >= 0) {
                 String dadosStr = new String(dadosBrutos, 0, qtdBytesLidos);
-                System.out.println(dadosStr);
                 qtdBytesLidos = in.read(dadosBrutos);
+//                System.out.println("Operação desejada: " + (new String(dadosBrutos, 0, 6)));
+//                System.out.println("CPF: " + (new String(dadosBrutos, 7, 17)));
+//                System.out.println("Nome: " + (new String(dadosBrutos, 18, 116)));
+//                System.out.println("endereço: " + (new String(dadosBrutos, 117, 226)));
+                System.out.println(dadosStr);
+                System.out.println(dadosStr.length());
+
             }
 
         } catch (UnknownHostException e) {
