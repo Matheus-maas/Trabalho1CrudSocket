ead126722a9c2e846130e33403186cf1c84b745e
matheus-maas <matheus.h.maas@hotmail.com>
2021-06-06 10:39:06 -0300

    alterações

--- Trabalho1CrudSocket/src/control/Servidor.java
+++ Trabalho1CrudSocket/src/control/Servidor.java
@@ -5,9 +5,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 
 public class Servidor {
+   
+    static String dadosStr=null; 
+    static ArrayList<String> dadosPessoas = new ArrayList<>();
+    
+    public static void inserir(String dados){
+        dadosPessoas.add(dados);
+    
+    }
+    
+    public static void mostrarDados() throws IOException{
+        Conexao c=new Conexao();
+        String auxiliar=null;
+        for(int i =0; i< dadosPessoas.size(); i++){
+        auxiliar=auxiliar+dadosPessoas.get(i);
+        System.out.println(auxiliar);
+        c.enviarDados(auxiliar);
+         }
+            
+    }
 
     public static void main(String[] args) throws IOException{
        System.out.println("Criando Conexão....");
@@ -19,9 +39,10 @@ public class Servidor {
             byte[] dadosBrutos = new byte[1024];
             int qtdBytesLidos = in.read(dadosBrutos);
             while (qtdBytesLidos >= 0) {
-                String dadosStr = new String(dadosBrutos, 0, qtdBytesLidos);
+                dadosStr = new String(dadosBrutos, 0, qtdBytesLidos);
                 System.out.println(dadosStr);
                 qtdBytesLidos = in.read(dadosBrutos);
+                inserir(dadosStr);
             }
 
         } catch (UnknownHostException e) {
